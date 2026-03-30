6da297851613fb388887594938db85fdaf15a93b
Kevin Koslowski <kevin@madwizard.de>
2026-03-30 11:48:49 +0000

    desomnia 3.0.0-alpha50
    Closes #20.
    
    Signed-off-by: Kevin Koslowski <kevin@madwizard.de>

--- Formula/desomnia.rb
+++ Formula/desomnia.rb
@@ -1,8 +1,8 @@
 class Desomnia < Formula
   desc "Daemon for sleep and resource management"
   homepage "https://github.com/mad0x20wizard/Desomnia"
-  url "https://github.com/mad0x20wizard/Desomnia/archive/refs/tags/v3.0.0-alpha48.tar.gz"
-  sha256 "9be3e5babbfd853584d43696f4f465d07a2469a9fe9ec6f0a807a64db8327b90"
+  url "https://github.com/mad0x20wizard/Desomnia/archive/refs/tags/v3.0.0-alpha50.tar.gz"
+  sha256 "e57b23c4bcd028889069b0b350067c41076131aa762dc968e5b8a4d0b5eb8545"
   license "GPL-3.0-or-later"
 
   bottle do
