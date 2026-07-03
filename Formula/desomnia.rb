5d2b8c9beb100d1328db01e22989cdcc4d8c8ec2
Kevin Koslowski <kevin@madwizard.de>
2026-07-03 20:13:40 +0000

    desomnia 3.1.0-alpha6

--- Formula/desomnia.rb
+++ Formula/desomnia.rb
@@ -1,8 +1,8 @@
 class Desomnia < Formula
   desc "Daemon for sleep and resource management"
   homepage "https://github.com/mad0x20wizard/Desomnia"
-  url "https://github.com/mad0x20wizard/Desomnia/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "5e312aa8ce255700a9efb87aa362dc685bc99892fd3c6caa6c0f2c41b3e5687c"
+  url "https://github.com/mad0x20wizard/Desomnia/archive/refs/tags/v3.1.0-alpha6.tar.gz"
+  sha256 "4ed3665a39c2377d002f5b2c6f032fd39aa357e54826ba6ab8be64e2a423c1a6"
   license "GPL-3.0-or-later"
 
   bottle do
